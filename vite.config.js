4526df6972fadf83e75548e62df386c33acaf5cf
Felix Berglund <fb222rs@student.lnu.se>
2026-05-15 15:47:25 +0200

    fix: exposte vite dev server on all network interfaces in docker:dev

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import cesium from 'vite-plugin-cesium'
 export default defineConfig({
   plugins: [cesium()],
   server: {
+    host: true,
     proxy: {
       '/api': 'http://localhost:3000',
     },
